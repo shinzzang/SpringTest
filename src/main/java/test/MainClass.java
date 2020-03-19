e97e4cd5939a4ddafba3e1afe8d427fa74f85313
shinzzang <asncyh1190@naver.com>
2020-03-19 21:33:13 +0900

    200319

--- src/main/java/test/MainClass.java
+++ src/main/java/test/MainClass.java
@@ -8,8 +8,10 @@ public class MainClass {
 		
 		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("SpringTestConfigure.xml");
 		Hello hello = ctx.getBean("helloTest",Hello.class);
+		Hello2 hello2 = ctx.getBean("helloTest2",Hello2.class);
 		
 		hello.sendHello();
+		hello2.sendHello();
 		
 		ctx.close();
 	}
