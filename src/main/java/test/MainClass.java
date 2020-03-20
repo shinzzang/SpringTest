1c6130364ef77edb65322ac76746e74395bb5a5e
shinzzang <asncyh1190@naver.com>
2020-03-20 11:38:33 +0900

    z

--- src/main/java/test/MainClass.java
+++ src/main/java/test/MainClass.java
@@ -2,8 +2,6 @@ package test;
 
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-import test2.Hello3;
-
 public class MainClass {
 	public static void main(String[] args) {
 		
@@ -11,11 +9,9 @@ public class MainClass {
 		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("SpringTestConfigure.xml");
 		Hello hello = ctx.getBean("helloTest",Hello.class);
 		Hello2 hello2 = ctx.getBean("helloTest2",Hello2.class);
-		Hello3 hello3 = ctx.getBean("helloTest3", Hello3.class);
 		
 		hello.sendHello();
 		hello2.sendHello();
-		hello3.sendHello();
 		
 		ctx.close();
 	}
